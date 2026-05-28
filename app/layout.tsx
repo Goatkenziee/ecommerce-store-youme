792b62338c59057bc1a00e541aff9d725b9e0053
Goatkenziee <mackquisition@gmail.com>
2026-05-27 21:56:42 -0400

    Update core Next.js configuration files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YouMe E-commerce",
-  description: "An e-commerce store built with Next.js and Prisma",
+  title: "YouMe E-commerce Store",
+  description: "An e-commerce store built with YouMe",
 };
 
 export default function RootLayout({
@@ -21,7 +21,9 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
-          <main className="p-4 max-w-7xl m-auto">{children}</main>
+          <main className="p-8">
+            {children}
+          </main>
         </body>
       </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
-import ProductCard from "@/components/ProductCard";
 import prisma from "@/lib/prisma";
-import { Metadata } from "next";
+import ProductCard from "@/components/ProductCard";
 
-interface HomePageProps {
-  searchParams: {
-    page: string;
-  };
-}
-
-export function generateMetadata(): Metadata {
-  return {
-    title: "YouMe E-commerce",
-    description: "An e-commerce store built with Next.js and Prisma",
-  };
-}
-
-export default async function Home({ searchParams }: HomePageProps) {
-  const currentPage = parseInt(searchParams.page) || 1;
-
-  const products = await prisma.product.findMany({
-    orderBy: { id: "desc" },
-  });
+export default async function Home() {
+  const products = await prisma.product.findMany();
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {products.map((product) => (
-        <ProductCard product={product} key={product.id} />
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
